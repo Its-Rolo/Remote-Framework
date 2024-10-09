8d474c3ad8c0a8b755891af2ee10497821fd0885
EMonkey895 <emonkey178@gmail.com>
2024-10-09 15:41:04 -0400

    Added progress bar for auto detection

--- RemoteFramework.py
+++ RemoteFramework.py
@@ -30,11 +30,11 @@ def auto_detection():
     timeout = input("\n Please input the delay between each scan, higher values will result in a slower scan but lower values may skip over the device: ")
     startingValue = input("Input the starting value for the scan (Scans from top down): ")
     # Iterate through the last octet (3 digits) from 255 to 0
-    for i in range(int(startingValue), -1, -1):
+    for i in tqdm(range(int(startingValue), -1, -1)):
         # Construct the full IP address
         ip = f"{base_ip}.{i}"
         url = f"http://{ip}:8060/query/device-info"
-
+        
         if i == 0:
             print("No device found.")
             exit()
@@ -43,7 +43,9 @@ def auto_detection():
             # Send a request to the Roku device info endpoint
             response = requests.get(url, timeout=float(timeout))  # Timeout set to 0.5 seconds to speed up the scan
             if response.status_code == 200:
+                clear_console()
                 print(f"Possible device found at: {ip}")
+                input("Press enter to continue.")
                 continue
         except requests.ConnectionError:
             # If the connection fails, just continue to the next IP
